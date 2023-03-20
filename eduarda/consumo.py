18a69b8486c5f1c8c29de7756ac974362e80ff89
JapaProgramadora <76882368+JapaProgramadora@users.noreply.github.com>
2023-03-20 09:54:31 -0300

    japalinda commits

---
+++ eduarda/consumo.py
@@ -0,0 +1,5 @@
+distance = int(input())
+combustivel = float(input())
+
+total = distance/combustivel
+print(f"{total:.3f} km/l")
---
+++ eduarda/tamanho.py
@@ -0,0 +1,10 @@
+numeros = input()
+numeros_list = numeros.split(' ')
+
+maior = 0
+
+for num in numeros_list:
+    if(float(num) > maior):
+        maior = float(num)
+
+print(maior)
